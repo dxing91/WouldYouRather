7e9a694c1deb0b419f7e32b8b96474fb7b6b95fd
dxing <domett.xing@gmail.com>
2017-08-16 23:53:21 +1000

    project set up

---
+++ app/containers/Main/MainContainer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class MainContainer extends Component {
+  render() {
+    return (
+      <p className='main'>Hello World!</p>
+    )
+  }
+}
---
+++ webpack.config.babel.js
@@ -0,0 +1,69 @@
+import webpack from 'webpack'
+import path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: path.join(__dirname, '/app/index.html'),
+  filename: 'index.html',
+  inject: 'body'
+})
+
+const ExtractTextPluginConfig = new ExtractTextPlugin('style-[hash].css')
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'dist')
+}
+
+const LAUNCH_COMMAND = process.env.npm_lifecycle
+
+const isProduction = LAUNCH_COMMAND === 'production'
+process.env.BABEL_ENV = LAUNCH_COMMAND
+
+const productionPlugin = new webpack.DefinePlugin({
+  'process.env': {
+    NODE_ENV: JSON.stringify('production')
+  }
+})
+
+const base = {
+  entry: [
+    PATHS.app
+  ],
+  output: {
+    path: PATHS.build,
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+      {test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader']}
+    ]
+  },
+  resolve: {
+    alias: {
+      style: path.resolve(__dirname, 'app/style'),
+      containers: path.resolve(__dirname, 'app/containers')
+    }
+  }
+}
+
+const developmentConfig = {
+  devtool: 'cheap-module-inline-source-map',
+  devServer: {
+    contentBase: PATHS.build,
+    hot: true,
+    inline: true
+  },
+  plugins: [HtmlWebpackPluginConfig, new webpack.HotModuleReplacementPlugin()]
+}
+
+const productionConfig = {
+  devtool: 'cheap-module-source-map',
+  plugins: [HtmlWebpackPluginConfig, productionPlugin]
+}
+
+export default Object.assign({}, base,
+  isProduction ? productionConfig : developmentConfig
+)
